b63fa9a2136239f73bc3fae48ec9d79a1eff7514
Rafael Rosetti <Rafa@MacBook-Pro-de-Rafael.local>
2019-07-19 15:40:53 -0300

    migration

---
+++ elibrary/database/migrations/2019_07_19_183938_create_venta.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVenta extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venta', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venta');
+    }
+}
